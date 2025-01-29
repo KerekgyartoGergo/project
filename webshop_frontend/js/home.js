a89ee07e57c702722c98c6193e598b4b6750e692
KerekgyartoGergo <kerekgyartogergo05@gmail.com>
2025-01-29 11:00:49 +0100

    Ákos faszságát kijavítani!

--- webshop_frontend/js/home.js
+++ webshop_frontend/js/home.js
@@ -1,6 +1,11 @@
 const btnLogout =document.getElementsByClassName ('icon-logout')[0];
+const btnProfile =document.getElementsByClassName ('icon-user')[0];
 
 
 btnLogout.addEventListener('click', ()=>{
     window.location.href='../webshop_frontend/index.html';
+});
+
+btnProfile.addEventListener('click', ()=>{
+    window.location.href='../webshop_frontend/profile.html';
 });
