df55fc9d74151465cb669151d182fd8abd1d87f9
KerekgyartoGergo <kerekgyartogergo05@gmail.com>
2025-02-04 09:55:07 +0100

    termékek lekérdezése, új termék feltöltése(csak admin)

--- webshop_backend/app.js
+++ webshop_backend/app.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const validator = require('validator');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -16,6 +18,10 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
+
+// az uploads mappában lévő fájlok elérése
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 
 dotenv.config();
@@ -34,6 +40,36 @@ const pool = mysql.createPool({
 });
 
 
+const uploadDir = 'uploads/';
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir)
+        }
+        cb(null, uploadDir);
+    },
+    filename: function(req, file, cb) {
+        const now = new Date().toISOString().split('T')[0];
+        cb(null, `${now}-${file.originalname}`);
+    }
+});
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 },
+    fileFilter: function(req, file, cb) {
+        const filetypes = /jpeg|jpg|png|gif|webp|avif/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
+
+        if(extname && mimetype) {
+            return cb(null, true);
+        } else {
+            cb(new Error('Csak képformátumok megengedettek!'));       
+        }
+    }
+});
+
+
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -43,11 +79,11 @@ function authenticateToken(req, res, next) {
         return res.status(403).json({ error: 'Nincs token' });
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             return res.status(403).json({ error: 'Van token, csak épp nem érvényes' });
         }
-        req.user_name = user;
+        req.user = decoded;
         next();
     });
 }
@@ -254,6 +290,56 @@ app.post('/api/adminRegister', (req, res) => {
                 res.status(201).json({ message: 'Admin fiók létrehozva!' });
             });
         });
+    });
+});
+
+
+
+// az összes termék lekérdezése
+app.get('/api/products', authenticateToken, (req, res) => {
+    const sql = 'SELECT * FROM products';
+
+    pool.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Hiba az SQL-ben', err });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ error: 'Nincs még termék' });
+        }
+
+        return res.status(200).json(result);
+    });
+});
+
+// új termek feltöltése
+app.post('/api/upload', authenticateToken, upload.single('pic'), (req, res) => {
+
+
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ error: 'Nincs jogosultságod termék feltöltésére' });
+    }
+
+    const pic = req.file ? req.file.filename : null;
+
+    if (pic === null) {
+        return res.status(400).json({ error: 'Válassz ki egy képet' });
+    }
+
+    const { name, description, price, stock, category_id } = req.body;
+
+    if (!name || !description || !price || !stock || !category_id) {
+        return res.status(400).json({ error: 'Minden mezőt ki kell tölteni' });
+    }
+
+    const sql = 'INSERT INTO products (product_id, name, description, price, stock, category_id, pic) VALUES (NULL, ?, ?, ?, ?, ?, ?)';
+    pool.query(sql, [name, description, price, stock, category_id, pic], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Hiba az SQL lekérdezésben' });
+        }
+
+        return res.status(201).json({ message: 'Termék sikeresen feltöltve', product_id: result.insertId });
     });
 });
 
