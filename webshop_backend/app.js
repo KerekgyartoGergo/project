1e2644055305aa7e95c717f6a5bd147d0646b5e1
KerekgyartoGergo <kerekgyartogergo05@gmail.com>
2025-02-21 12:46:07 +0100

    asd

--- webshop_backend/app.js
+++ webshop_backend/app.js
@@ -853,12 +853,12 @@ app.post('/api/updateItem', authenticateToken, upload.single('pic'), (req, res) 
     });
 });
  //termék szerkesztése2
- app.post('/api/updateProductsInfo', authenticateToken, upload.single('pic'), (req, res) => {
+ app.post('/api/updateProductsInfo', authenticateToken, (req, res) => {
+
     if (req.user.role !== 'admin') {
-        return res.status(403).json({ error: 'Nincs jogosultságod termék frissítésére' });
+        return res.status(403).json({ error: 'Nincs jogosultságod terméfrissítésére' });
     }
 
-    const pic = req.file ? req.file.filename : null;
 
     const { Jelátvitel, Max_működési_idő, Hordhatósági_változatok, Termék_típusa, Kivitel, Bluetooth_verzió, Hangszóró_meghajtók, Szín, Csatlakozók, Bluetooth, Frekvenciaátvitel, Érzékenység, id } = req.body;
 
